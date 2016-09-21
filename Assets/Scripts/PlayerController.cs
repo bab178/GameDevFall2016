4e55b2c7cae6d2343700d0a92822c6c9943c7034
Blake Bordovsky <bab178@txstate.edu>
2016-09-21 15:40:05 -0500

    -Create basic PlayerController -Create empty scene MainScene -Create GameDevFall2016.exe build -Add to .gitignore

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Player player;
+    public Rigidbody2D rb;
+    public int CustomHealth;
+    public int CustomSpeed;
+
+
+    [System.Serializable]
+    public class Player
+    {
+        public Stats stats;
+
+        // Default Contructor
+        public Player(int hp, int spd)
+        {
+            stats = new Stats(hp, spd);
+        }
+    }
+
+    [System.Serializable]
+    public class Stats
+    {
+        public int Health;
+        public int Speed;
+
+        // Default Contructor
+        public Stats()
+        {
+            Health = 10;
+            Speed = 5;
+        }
+
+        public Stats(int hp, int spd)
+        {
+            // Validate hp and spd
+            if(hp > 0 && spd > 0)
+            {
+                Health = hp;
+                Speed = spd;
+            }
+            else
+            {
+                Health = 10;
+                Speed = 5;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        player = new Player(CustomHealth, CustomSpeed);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorzontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorzontal * player.stats.Speed, moveVertical * player.stats.Speed);
+        rb.velocity = movement;
+    }
+}
+
