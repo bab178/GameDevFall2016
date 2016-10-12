0f62bac5e4ae62bf17164d531361e48d60880a0c
DESKTOP-AQLJ8AO\Njahni Richards <njrich00@gmail.com>
2016-10-11 19:49:53 -0500

    #7 Made a Health Bar on GUI

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+    publick slider HealthBar;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
