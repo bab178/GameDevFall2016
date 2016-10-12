cf179ddda8bbb5aa257de20670e3ea401751adb8
DESKTOP-AQLJ8AO\Njahni Richards <njrich00@gmail.com>
2016-10-11 20:09:35 -0500

    #7 We Got  A  Freakin Health Bar!!

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,15 +5,27 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    publick slider HealthBar;
+    public Slider healthBar;
+    public float maxHealth;
+    public PlayerController Player;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        maxHealth = Player.PlayerStats.Health;
+        healthBar.value = maxHealth;
+        healthBar.minValue = 0;
+        healthBar.maxValue = maxHealth;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        UpdateHealth();
 	}
+
+    void UpdateHealth ()
+    {
+        healthBar.value = Player.PlayerStats.Health;
+    }
 }
