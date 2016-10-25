5e1e5f5b7af47c231c59b45baf0cb674a9836367
KEVIN\Kevin <kimkevin7704@gmail.com>
2016-10-25 17:51:48 -0500

    #6: Red dude obstacles make player dead

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+    public enum ObstacleType { DeathTrap, BurnTrap, KnockbackTrap };
+
+    public ObstacleType obstacleType;
+
+    public int Magnitude;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerController playerController = other.gameObject.GetComponent<PlayerController>();
+
+            if (obstacleType == ObstacleType.DeathTrap)
+            {
+                playerController.TakeDamage(1000);
+            }
+            else if (obstacleType == ObstacleType.BurnTrap)
+            {
+                playerController.PlayerStats.Health -= Magnitude;
+            }
+            else if (obstacleType == ObstacleType.KnockbackTrap)
+            {
+
+            }
+        }
+    }
+}
